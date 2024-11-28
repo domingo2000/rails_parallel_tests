3514a39262b4bf5f1bbbc48146579e7a7cfeced5
domingo2000 <dedwards@buk.cl>
2024-11-27 23:42:53 -0300

    test: reduce test number to 20

--- test/load/load_test_31_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-require "active_support/test_case"
-
-class LoadTest31 < ActiveSupport::TestCase
-  test "dummy test 31" do
-    sleep(1)
-    assert_equal 2, 1 + 1
-  end
-end
--- test/load/load_test_72_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-require "active_support/test_case"
-
-class LoadTest72 < ActiveSupport::TestCase
-  test "dummy test 72" do
-    sleep(1)
-    assert_equal 2, 1 + 1
-  end
-end
