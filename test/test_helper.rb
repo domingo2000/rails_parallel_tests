39a4c24b2dfae66f6aef715abbb5108fe7d7d555
domingo2000 <dedwards@buk.cl>
2024-11-27 23:39:50 -0300

    chore: add coverage reprots with parallell execution nodes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,22 @@
+
 ENV["RAILS_ENV"] ||= "test"
+
+require "simplecov"
+SimpleCov.start "rails" do
+  command_name "Job #{ENV["TEST_ENV_NUMBER"]}" if ENV["TEST_ENV_NUMBER"]
+
+  if ENV["RECORD_RUNTIME"]
+    formatter SimpleCov::Formatter::SimpleFormatter
+  else
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+      SimpleCov::Formatter::SimpleFormatter,
+      SimpleCov::Formatter::HTMLFormatter
+    ])
+  end
+
+  track_files "**/*.rb"
+end
+
 require_relative "../config/environment"
 require "rails/test_help"
 require "parallel_tests/test/runtime_logger" if ENV["RECORD_RUNTIME"]
